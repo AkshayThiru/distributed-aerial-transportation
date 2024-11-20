2e8a5b4af7f096e296a0fd9da16ee2645d5907ec
AkshayThiru <akshay_t@berkeley.edu>
2024-11-20 14:24:39 -0800

    Add unit tests for n > 3 for system dynamics

--- test/system/test_pmrldynamics.py
+++ test/system/test_pmrldynamics.py
@@ -5,35 +5,34 @@ from system.point_mass_rigid_link import (PMRLDynamics, PMRLParameters,
                                           PMRLState)
 
 
-def _pmrl_3robot_parameters() -> PMRLParameters:
-    m = np.array([0.5] * 3)
+def _pmrl_nrobot_parameters(n: int) -> PMRLParameters:
+    m = np.array([0.5] * n)
     ml = 0.225
     Jl = np.diag([2.1, 1.87, 3.97]) * 1e-2
-    r = np.vstack(
-        [
-            np.array([-0.42, -0.27, 0]),
-            np.array([0.48, -0.27, 0]),
-            np.array([-0.06, 0.55, 0]),
-        ]
-    ).T
-    L = np.array([1.0] * 3)
+    if n == 3:
+        r = np.vstack(
+            [
+                np.array([-0.42, -0.27, 0]),
+                np.array([0.48, -0.27, 0]),
+                np.array([-0.06, 0.55, 0]),
+            ]
+        ).T
+    else:
+        r = np.random.random((3, n))
+    L = np.array([1.0] * n)
     return PMRLParameters(m, ml, Jl, r, L)
 
 
-def _pmrl_3robots_init_state() -> PMRLState:
+def _pmrl_nrobots_init_state(n: int) -> PMRLState:
     q = np.vstack(
         [
             np.array([0.0, 0.0, 1.0]),
-            np.array([0.0, 0.0, 1.0]),
-            np.array([0.0, 0.0, 1.0]),
-        ]
+        ] * n
     ).T
     dq = np.vstack(
         [
             np.array([0.0, 0.0, 0.0]),
-            np.array([0.0, 0.0, 0.0]),
-            np.array([0.0, 0.0, 0.0]),
-        ]
+        ] * n
     ).T
     xl = np.array([0.0, 0.0, 0.0])
     vl = np.array([0.0, 0.0, 0.0])
@@ -42,26 +41,31 @@ def _pmrl_3robots_init_state() -> PMRLState:
     return PMRLState(q, dq, xl, vl, Rl, wl)
 
 
-def _pmrl_3robot_force_trajectory(p: PMRLParameters, t: float) -> np.ndarray:
+def _pmrl_nrobot_force_trajectory(p: PMRLParameters, t: float, n: int) -> np.ndarray:
     total_mass = p.m.sum() + p.ml
-    f_ = np.array([0, 0, 1]) * total_mass * constants.g / 3
-    k1_d, k2_d = total_mass * constants.g / 5, total_mass * constants.g / 5
-    f1_d, f2_d = np.pi / 2, np.pi
-    f_ = f_ + np.array(
-        [k1_d * np.cos(f1_d * t), k1_d * np.sin(f1_d * t), k2_d * np.sin(f2_d * t)]
-    )
-    return np.vstack([f_] * 3).T
+    if n == 3:
+        f_ = np.array([0, 0, 1]) * total_mass * constants.g / 3
+        k1_d, k2_d = total_mass * constants.g / 5, total_mass * constants.g / 5
+        f1_d, f2_d = np.pi / 2, np.pi
+        f_ = f_ + np.array(
+            [k1_d * np.cos(f1_d * t), k1_d * np.sin(f1_d * t), k2_d * np.sin(f2_d * t)]
+        )
+        f = np.vstack([f_] * 3).T
+    else:
+        f = np.random.random((3, n)) * total_mass * constants.g / n
+    return f
 
 
 def main() -> None:
+    n = 4
     dt = 5e-3
     t_seq = np.arange(0, 10, dt)
     dyn_err_seq = np.empty(t_seq.shape)
-    s0 = _pmrl_3robots_init_state()
-    p = _pmrl_3robot_parameters()
+    s0 = _pmrl_nrobots_init_state(n)
+    p = _pmrl_nrobot_parameters(n)
     dyn = PMRLDynamics(p, s0, dt)
     for i in range(len(t_seq)):
-        f = _pmrl_3robot_force_trajectory(p, t_seq[i])
+        f = _pmrl_nrobot_force_trajectory(p, t_seq[i], n)
         acc, T = dyn.forward_dynamics(f)
         dyn_err_seq[i] = PMRLDynamics.inverse_dynamics_error(dyn.state, p, f, T, acc)
         dyn.integrate(f)
--- test/system/test_rpdynamics.py
+++ test/system/test_rpdynamics.py
@@ -4,20 +4,23 @@ from scipy import constants
 from system.rigid_payload import RPDynamics, RPParameters, RPState
 
 
-def _rp_3actuators_parameters() -> RPParameters:
+def _rp_nactuators_parameters(n: int) -> RPParameters:
     ml = 0.225
     Jl = np.diag([2.1, 1.87, 3.97]) * 1e-2
-    r = np.vstack(
-        [
-            np.array([-0.42, -0.27, 0]),
-            np.array([0.48, -0.27, 0]),
-            np.array([-0.06, 0.55, 0]),
-        ]
-    ).T
+    if n == 3:
+        r = np.vstack(
+            [
+                np.array([-0.42, -0.27, 0]),
+                np.array([0.48, -0.27, 0]),
+                np.array([-0.06, 0.55, 0]),
+            ]
+        ).T
+    else:
+        r = (np.random.random((3, n)) - 0.5) * 2.0
     return RPParameters(ml, Jl, r)
 
 
-def _rp_3actuators_init_state() -> RPState:
+def _rp_nactuators_init_state(n: int) -> RPState:
     xl = np.array([0.0, 0.0, 0.0])
     vl = np.array([0.0, 0.0, 0.0])
     Rl = np.eye(3)
@@ -25,25 +28,30 @@ def _rp_3actuators_init_state() -> RPState:
     return RPState(xl, vl, Rl, wl)
 
 
-def _rp_3actuators_force_trajectory(p: RPParameters, t: float) -> np.ndarray:
-    f_ = np.array([0, 0, 1]) * p.ml * constants.g / 3
-    k1_d, k2_d = p.ml * constants.g / 5, p.ml * constants.g / 5
-    f1_d, f2_d = np.pi / 2, np.pi
-    f_ = f_ + np.array(
-        [k1_d * np.cos(f1_d * t), k1_d * np.sin(f1_d * t), k2_d * np.sin(f2_d * t)]
-    )
-    return np.vstack([f_] * 3).T
+def _rp_nactuators_force_trajectory(p: RPParameters, t: float, n: int) -> np.ndarray:
+    if n == 3:
+        f_ = np.array([0, 0, 1]) * p.ml * constants.g / 3
+        k1_d, k2_d = p.ml * constants.g / 5, p.ml * constants.g / 5
+        f1_d, f2_d = np.pi / 2, np.pi
+        f_ = f_ + np.array(
+            [k1_d * np.cos(f1_d * t), k1_d * np.sin(f1_d * t), k2_d * np.sin(f2_d * t)]
+        )
+        f = np.vstack([f_] * 3).T
+    else:
+        f = np.random.random((3, n)) * p.ml * constants.g / n
+    return f
 
 
 def main() -> None:
+    n = 4
     dt = 5e-3
     t_seq = np.arange(0, 10, dt)
     dyn_err_seq = np.empty(t_seq.shape)
-    s0 = _rp_3actuators_init_state()
-    p = _rp_3actuators_parameters()
+    s0 = _rp_nactuators_init_state(n)
+    p = _rp_nactuators_parameters(n)
     dyn = RPDynamics(p, s0, dt)
     for i in range(len(t_seq)):
-        f = _rp_3actuators_force_trajectory(p, t_seq[i])
+        f = _rp_nactuators_force_trajectory(p, t_seq[i], n)
         acc = dyn.forward_dynamics(f)
         dyn_err_seq[i] = RPDynamics.inverse_dynamics_error(dyn.state, p, f, acc)
         dyn.integrate(f)
